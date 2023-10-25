ba3040a1168538bfb2ce2ee02f1569bc9240921f
ponjasma <ponjasma@gmail.com>
2023-10-25 20:06:31 -0300

    centre el nav

--- js/header.js
+++ js/header.js
@@ -2,8 +2,8 @@ function add_main_header(id) {
     document.getElementById(id).innerHTML = 
                 `<div style="width: 222px; max-width: 25%;"> 
                     <img src="img/logo2.jpg" style="object-fit: contain; width: 100%;"> 
-                </div>
-                <nav>
+                </div>    
+                <nav style="align-self: center;">
                     <a href="index.html">Home</a>
                     <a href="nosotros.html">Nosotros</a>
                     <a href="productos.html">Productos</a>
